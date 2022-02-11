0817ae483d198c5e932ea29e84a312cf1f8b46ca
‘Roux <amandine92.roux@gmail.com>
2022-02-11 15:13:42 +0100

    seconde version

--- Game.py
+++ Game.py
@@ -1,18 +1,34 @@
-class Game:
+class Game():
 
-    def __init__(self, nom, context, butin, team):
-        self.__nom = nom
-        self.__context = context
-        self.__butin = butin
-        self.__team = team
+    def __init__(self):
+        print("bonjour ! ceci est un essai")
     
     def config(self):
         self.__nom = input("Entrer le nom du joueur : ")
+        print("ok config")
     
     def start(self):
         self.__context = "mouvement"
         self.__butin = 40
         self.__team = []
+        print("ok start")
         
     def status(self):
+        if(self.__butin == 0):
+            print("GAME OVER")
+        else:
+            print(self.__nom)
+            print(self.__butin)
+            # i = 0
+            # while i<=self.__team.__len__():
+            #     print()
+            
+            print("ok status")
+            
+            
+if __name__ == '__main__':
+    g = Game()
+    g.config()
+    g.start()
+    g.status()
         
