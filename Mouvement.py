d486b5f0e3cb268ad8a39401e6996c3255771418
Abbes Nourhene <abbesnourhene@MacBook-Air-de-Abbes.local>
2022-02-11 15:15:06 +0100

    ce ficher contient le code pour les mouvements

---
+++ Mouvement.py
@@ -0,0 +1,13 @@
+class Mouvement:
+    d='aller à droite'
+    g='aller à gauche'
+    h='aller en haut'
+    b='aller en bas'
+def direction_d (self,d):
+    print("d")
+def direction_g (self,g):
+    print("g")
+def direction_h (self,h):
+    print("h")
+def direction_b (self,b):
+    print("b")
